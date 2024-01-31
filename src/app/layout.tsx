2ad65b5d8d8a8e2cd3a0476ec5e6a25ca84b0729
carlos <carloslonghi.cl@gmail.com>
2024-01-30 23:37:48 -0300

    create nav and style scroll bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,45 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header></header>
-        <main className="bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-100">
+        <header className="flex justify-center bg-gray-100 px-4 py-6 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+          <nav>
+            <ul className="flex max-w-app items-center justify-between gap-4 md:gap-6">
+              <li>
+                <a
+                  className="rounded px-3 py-2 text-cyan-100 transition duration-300 ease-out hover:text-cyan-400 hover:shadow-lg hover:shadow-cyan-950 hover:dark:bg-gray-950"
+                  href="/"
+                >
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <a
+                  className="rounded px-3 py-2 text-cyan-100 transition duration-300 ease-out hover:text-cyan-400 hover:shadow-lg hover:shadow-cyan-950 hover:dark:bg-gray-950"
+                  href="/projects"
+                >
+                  Projetos
+                </a>
+              </li>
+              <li>
+                <a
+                  className="rounded px-3 py-2 text-cyan-100 transition duration-300 ease-out hover:text-cyan-400 hover:shadow-lg hover:shadow-cyan-950 hover:dark:bg-gray-950"
+                  href="/about"
+                >
+                  Carreira
+                </a>
+              </li>
+              <li>
+                <a
+                  className="rounded px-3 py-2 text-cyan-100 transition duration-300 ease-out hover:text-cyan-400 hover:shadow-lg hover:shadow-cyan-950 hover:dark:bg-gray-950"
+                  href="/contact"
+                >
+                  Contato
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="overflow-x-hidden bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-100">
           {children}
         </main>
       </body>
