4ebf86851dd8dce829066cf0607c43a14f2cdcc3
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-04-17 09:39:11 -0300

    fix: fix error "Uncaught (in promise) Timeout recaptcha"

--- src/components/DownloadCurriculum.tsx
+++ src/components/DownloadCurriculum.tsx
@@ -4,13 +4,17 @@ import ReCAPTCHA from 'react-google-recaptcha'
 
 export function DownloadCurriculum() {
   const reCaptchaSiteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''
-  const [captcha, setCaptcha] = useState<string | null>('')
+  const [captcha, setCaptcha] = useState<string | null>(null)
 
   return (
     <>
-      {!captcha ? (
-        <ReCAPTCHA sitekey={reCaptchaSiteKey} onChange={setCaptcha} />
-      ) : (
+      <ReCAPTCHA
+        sitekey={reCaptchaSiteKey}
+        onChange={setCaptcha}
+        hidden={captcha !== null}
+      />
+
+      {captcha && (
         <a
           className="flex cursor-pointer items-center justify-center gap-2 rounded-md bg-cyan-900 py-3 md:w-1/2"
           href="pdf/CarlosLonghi_CV.pdf"
